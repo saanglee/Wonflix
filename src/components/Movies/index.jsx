59c226186cbf2230b146a7108c8404ff7d716ecf
dev-redo <mis05041@naver.com>
2022-07-09 18:42:37 +0900

    style: Move modal to render in Movies instead of Home
    영화 게시물의 모달을 Home페이지가 아닌 영화 게시물 리스트인 movies에서 관리하는 것이
    옳다고 판단해 이동시켜주었다.

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -3,8 +3,9 @@ import { useRecoilState } from 'recoil';
 import _ from 'lodash';
 import { useGetAllMovies } from '../../api/useGetMovie';
 import { inputState, focusedInput, moviesData } from '../../store/search';
-import Card from './Card/Card';
 import './movies.scss';
+import Card from './Card/Card';
+import Modal from '../Modal';
 
 const Movies = () => {
   const { data } = useGetAllMovies();
@@ -16,13 +17,14 @@ const Movies = () => {
   }, [data]);
 
   return (
-      <>
-          <div className='movies'>
-              {movies?.map((movie) => (
-                  <Card key={movie.id} movie={movie} role='presentation' />
+    <>
+      <div className='movies'>
+        {movies?.map((movie) => (
+          <Card key={movie.id} movie={movie} role='presentation' />
         ))}
-          </div>
-      </>
+      </div>
+      <Modal />
+    </>
   );
 };
 
