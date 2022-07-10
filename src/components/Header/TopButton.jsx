ca2dad7d1dc100b76f2b47004c2789ae1fad9cf5
manjyuriah <manjyuriah01@gmail.com>
2022-07-10 14:44:01 +0900

    style 탑버튼 svg삽입 및 css수정

--- src/components/Header/TopButton.jsx
+++ src/components/Header/TopButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './topButton.scss';
 import {moveToTop} from '../../hooks/moveToTop';
+import { ReactComponent as MoveToTopIcon } from '../../assets/svgs/move_to_top.svg';
 const TopButton = () => {
     return (
         
         <div className='top-button'>
             <button className='top-btn' onClick={moveToTop}>
-             ⬆️
+             <MoveToTopIcon />
                 </button>
         </div>
     );
