b93a171df844f34f49772cc3faeac5185afc127d
dev-redo <mis05041@naver.com>
2022-07-13 03:07:06 +0900

    fix: 즐겨찾기 페이지 새로고침 에러 해결
    백엔드에서 raw data를 전달해주었다는 가정하에 App.js에서 데이터를 전부 받아오는 방식으로 에러를 해결하였다.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import styles from './Routes.module.scss';
 import { Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import { useSetRecoilState } from 'recoil';
+import { useGetAllMovies } from '../api/useGetMovie';
 import { ModalContextProvider } from '../store/modal';
+import { moviesData } from '../store/movies';
 
 import Layout from './Layout/Layout';
 import Home from './Home/Home';
@@ -9,18 +12,24 @@ import Favorites from '../components/Favorites/Favorites';
 import NotFound from './NotFound404/NotFound';
 
 const App = () => {
+  const { data } = useGetAllMovies();
+  const setMovies = useSetRecoilState(moviesData);
+
+  React.useEffect(() => {
+    if (data === undefined || data === null) return;
+    setMovies(data);
+  }, [data]);
+
   return (
     <ModalContextProvider>
       <div className={styles.app}>
-        <RecoilRoot>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path='favorites' element={<Favorites />} />
-              <Route path='*' element={<NotFound />} />
-            </Route>
-          </Routes>
-        </RecoilRoot>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='favorites' element={<Favorites />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
       </div>
     </ModalContextProvider>
   );
