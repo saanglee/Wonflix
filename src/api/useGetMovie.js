ef472d66bd44404dca07c4554e939fab62e582fe
soob <1008sb354@gmail.com>
2022-07-12 22:20:23 +0900

    [#55]fix: change api structure

--- src/api/useGetMovie.js
+++ src/api/useGetMovie.js
@@ -1,7 +1,11 @@
-import { useFetchData } from './useFetchData';
+import axios from 'axios';
 import { BASE_URL } from './config';
 
-export const useGetAllMovies = () => {
-  const response = useFetchData(BASE_URL);
-  return response.data;
+export const useGetAllMovies = async () => {
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
