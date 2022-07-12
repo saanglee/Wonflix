a12c58c9e9d9005b739f55ad620c5b6d15d6ef25
dev-redo <mis05041@naver.com>
2022-07-12 10:48:36 +0900

    feat: 게시물, 모달에 즐겨찾기 api 연동

--- src/components/Movies/Card/Card.jsx
+++ src/components/Movies/Card/Card.jsx
@@ -1,71 +1,74 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import { useRecoilState } from 'recoil';
-import { useModifyModal } from '../../../store/modal';
-import './card.scss';
-import { CheckIcon } from '../../../assets/svgs/index';
 import ModalContent from '../ModalContent/ModalContent';
-import { useDebounce } from '../../../hooks/useDebounce';
+import { useModifyModal } from '../../../store/modal';
 import { moviesData } from '../../../store/movies';
+import { useDebounce } from '../../../hooks/useDebounce';
 import { useUpdateFavorite } from '../../../api/useUpdateFavorite';
-import { StarEmptyIcon, StarFilledIcon } from '../../../assets/svgs/index';
+import { replaceItemAtIndex } from '../../../util/replaceItemAtIndex';
+import './card.scss';
 
-const Card = ({ movie, ...props }) => {
-  const favoriteInput = useRef(null);
-  const [movieData, setmovieData] = useState(movie);
-
-  const {
-    id,
-    imdb_code,
-    title,
-    title_english,
-    title_long,
-    language,
-    rating,
-    runtime,
-    summary,
-    synopsis,
-    medium_cover_image,
-    large_cover_image,
-    like,
-  } = movieData;
-
+const Card = ({ movie }) => {
   const { openModal } = useModifyModal();
+  const [movies, setMovies] = useRecoilState(moviesData);
 
   const openModalWithData = () =>
     openModal({
       children: <ModalContent movie={movie} />,
-      onSubmit: () => console.log('submit'), // TODO: 클라이언트 즐겨찾기 Toggle api
+      onSubmit: (movie) => toggleFavorite(movie),
     });
 
-  const onChangeFavorite = (_id) => {
-    const isChecked = favoriteInput.current.checked;
-    useUpdateFavorite(id, isChecked).then((result) => setmovieData(result));
+  // TODO: id를 전달받아 그 값만 갱신하는 selector 찾기
+  // => 없으면 그냥 진행
+  const toggleFavorite = async (movie) => {
+    const index = movies.findIndex((movieData) => movieData.id === movie.id);
+    let updateMovies = replaceItemAtIndex(movies, index, {
+      ...movie,
+      like: !movie.like,
+    });
+    setMovies(updateMovies);
+
+    const response = await useUpdateFavorite(movie);
+
+    if (response.ok) return;
+    updateMovies = replaceItemAtIndex(movies, index, {
+      ...movie,
+      like: !movie.like,
+    });
+    setMovies(updateMovies);
   };
 
   return (
     <>
       <div className='card'>
-        <div className='card_img' onClick={() => openModalWithData(movieData)}>
-          <img src={medium_cover_image} alt={title} />
+        <div className='card_img' onClick={openModalWithData}>
+          <img src={movie.medium_cover_image} alt={movie.title} />
         </div>
-        <h1 className='card_title' onClick={() => openModalWithData(movieData)}>
-          {title}
+        <h1 className='card_title' onClick={openModalWithData}>
+          {movie.title}
         </h1>
         <div className='card_favor'>
           <input
             type='checkbox'
             name='favor'
-            id={`favor${id}`}
-            ref={favoriteInput}
-            onChange={() => onChangeFavorite(id)}
-            checked={like}
+            id={`favor${movie.id}`}
+            onChange={() => toggleFavorite(movie)}
+            checked={movie.like}
           />
-          <label htmlFor={`favor${id}`}></label>
+          <label htmlFor={`favor${movie.id}`}></label>
         </div>
-        {/* <CheckIcon /> */}
       </div>
     </>
   );
 };
 
 export default React.memo(Card);
+
+// TODO: recoil에서 특정 값을 변경할 때 사용되는 util함수
+// => Object일 때 useRecoilState쓸 때 계속 쓰임 -> util로 이동
+
+// 하지만... 분명 하나만 수정하는 방식이 있을듯한데.. 진짜 selector 없나..?
+//  캐싱된다는데 대체 어떻게 캐싱되는거지...?
+// function replaceItemAtIndex(arr, index, newValue) {
+//   return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+// }
---
+++ src/util/replaceItemAtIndex.js
@@ -0,0 +1,5 @@
+const replaceItemAtIndex = (arr, index, newValue) => {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+};
+
+export { replaceItemAtIndex };
