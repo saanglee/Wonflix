06adf3556385624269126cf19e64e7dfa657b9ad
dev-redo <mis05041@naver.com>
2022-07-12 18:42:00 +0900

    fix: Fix Favorites Page Re-rendering Error
    즐겨찾기 페이지가 재렌더링 되지 않는 이슈를 recoil의 selector을 이용해 해결하였다.
    
    close #61

--- src/store/movies.js
+++ src/store/movies.js
@@ -1,6 +1,14 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const moviesData = atom({
   key: 'moviesData',
   default: [],
+});
+
+export const favoriteMoviesData = selector({
+  key: 'favoriteMoviesData',
+  get: ({ get }) => {
+    const movies = get(moviesData);
+    return movies.filter((movie) => movie.like);
+  },
 });
